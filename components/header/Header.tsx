3e80ab8273c853de8b43c48808d5e68a977c0e1d
AlaaTaha <toqa8854@gmail.com>
2024-12-13 07:31:01 -0800

    first1

--- components/header/Header.tsx
+++ components/header/Header.tsx
@@ -1,9 +1,4 @@
 'use client';
-import { Swiper, SwiperSlide } from 'swiper/react';
-import 'swiper/css';
-import 'swiper/css/pagination';
-import 'swiper/css/navigation';
-import { Pagination, Navigation } from 'swiper/modules';
 import { FaArrowRightLong, FaArrowLeftLong } from "react-icons/fa6";
 import useHoodieStore from '@/lib/hoodieStore'; // استدعاء Zustand store
 import HoodiViewer from './HoodiViewer';
@@ -21,8 +16,16 @@ export default function Header() {
     setCurrentHoodie,
   } = useHoodieStore();
 
-  const handleSlideChange = (swiper: any) => {
-    setCurrentHoodie(swiper.activeIndex);
+  const handlePrevClick = () => {
+    if (!isAtStart) {
+      setCurrentHoodie(currentHoodie - 1);
+    }
+  };
+
+  const handleNextClick = () => {
+    if (!isAtEnd) {
+      setCurrentHoodie(currentHoodie + 1);
+    }
   };
 
   return (
@@ -32,48 +35,29 @@ export default function Header() {
           isAtStart ? 'opacity-50' : 'opacity-100'
         }`}
         id="prev-button"
+        onClick={handlePrevClick}
         disabled={isAtStart}
       >
         <FaArrowLeftLong />
       </button>
+
       <button
         className={`absolute right-5 bottom-[50px] z-10 text-white px-4 py-2 rounded transition-opacity duration-300 ${
           isAtEnd ? 'opacity-50' : 'opacity-100'
         }`}
         id="next-button"
+        onClick={handleNextClick}
         disabled={isAtEnd}
       >
         <FaArrowRightLong />
       </button>
+
       <NameNag />
-      <Swiper
-        spaceBetween={50}
-        slidesPerView={1}
-        pagination={{ clickable: true }}
-        className="w-[100%]"
-        autoplay={{ delay: 2500, disableOnInteraction: false }}
-        modules={[Pagination, Navigation]}
-        allowTouchMove={false}
-        navigation={{
-          prevEl: '#prev-button',
-          nextEl: '#next-button',
-        }}
-        onSlideChange={handleSlideChange}
-        style={{
-          '--swiper-navigation-color': '#fff',
-          '--swiper-pagination-color': 'red',
-          '--swiper-pagination-bottom': '50px',
-          '--swiper-pagination-bullet-inactive-color': 'red',
-          '--swiper-pagination-bullet-opacity': '0.6',
-        } as React.CSSProperties}
-      >
-        {hoodieList.map((_, index) => (
-          <SwiperSlide key={index}>
-            <HoodiViewer color={color} image={hoodieList[currentHoodie].image} />
-          </SwiperSlide>
-        ))}
-      </Swiper>
-      <Discription/>
+      <div className="w-full flex justify-center items-center">
+        <HoodiViewer color={color} image={hoodieList[currentHoodie].image} />
+      </div>
+
+      <Discription />
       <Social />
     </div>
   );
