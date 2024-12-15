8fbc659769f07b428b51351f1eb0c522417ed656
AlaaTaha <toqa8854@gmail.com>
2024-12-15 12:20:32 -0800

    first32

--- app/page.tsx
+++ app/page.tsx
@@ -5,23 +5,13 @@ import { useGLTF, useProgress } from '@react-three/drei';
 import Header from '@/components/header/Header';
 
 export default function Home() {
-  const [isLoading, setIsLoading] = useState(true);
   useGLTF.preload('/hoodie.glb') as any;
   const { progress } = useProgress();
-
-  useEffect(() => {
-    const timer = setTimeout(() => {
-      setIsLoading(false);
-    }, 3000);
-
-    return () => clearTimeout(timer); // تنظيف المؤقت عند التفكيك
-  }, []);
-
   return (
     <>
       <PreLoader progress={progress} />
       <div>
-        {Math.round(progress) === 100 && !isLoading && (
+        {Math.round(progress) === 100 &&  (
           <Header />
         )}
       </div>
@@ -32,7 +22,7 @@ export default function Home() {
 /*
 git init
 git add .
-git commit -m "first30"
+git commit -m "first31"
 git branch -M main
 git remote add origin https://github.com/ToqaMohamedDev/nagaretto.git
 git push -u origin main
