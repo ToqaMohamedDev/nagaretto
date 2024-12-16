7bf1ff5ce96b169b218110f9ac9abb7d2d341b98
AlaaTaha <toqa8854@gmail.com>
2024-12-16 08:09:54 -0800

    first44

--- components/header/Discription.tsx
+++ components/header/Discription.tsx
@@ -4,11 +4,11 @@ import useHoodieStore from '@/lib/hoodieStore'; // Import the store
 export default function Discription() {
   const { currentHoodie, hoodieList } = useHoodieStore(); // Destructure the required variables from the store
   return (
-    <div className="absolute bottom-[120px] flex flex-col gap-1 items-center justify-center text-white z-10">
-      <p className="xl:text-[18px] lg:text-[16px] md:text-[14px] text-[12px] dark:text-gray-500 text-gray-700 font-semibold sm:w-[450px] w-[290px] text-center">
+    <div className="absolute bottom-[120px] left-5 flex flex-col gap-1  text-white z-10">
+      <p className="md:text-[14px] text-[12px] dark:text-white text-gray-700 font-semibold sm:w-[450px] w-[290px] text-left">
         {hoodieList[currentHoodie]?.description}
       </p>
-      <p className="xl:text-[22px] lg:text-[20px] md:text-[18px] text-[16px] dark:text-gray-500 text-gray-700 font-bold text-center">
+      <p className="xl:text-[22px] lg:text-[20px] md:text-[18px] text-[16px] dark:text-white text-gray-700 font-bold text-left">
         {hoodieList[currentHoodie]?.price}
       </p>
     </div>
